fcb76f593600f20f5456b952466caac99df1dcc6
Artur Danilov <artur.danilov@infoteam.de>
2021-10-20 17:27:15 +0200

    Connected the DB

--- UserApp/AuthWindow.xaml.cs
+++ UserApp/AuthWindow.xaml.cs
@@ -10,32 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace UserApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AuthWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
-
-        AppContext db;
-
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
-
-            db = new AppContext();
         }
 
-        private void BT_Register(object sender, RoutedEventArgs e)
+        private void BT_Auth_Click(object sender, RoutedEventArgs e)
         {
             string ID = TB_ID.Text.Trim();
             string pass = TB_Password.Password.Trim();
-            string pass_2 = TB_Password_2.Password.Trim();
-            string email = TB_EMail.Text.Trim().Trim();
 
             if (ID.Length < 5)
             {
@@ -47,32 +39,25 @@ namespace UserApp
                 TB_ID.ToolTip = "This field is entered incorrectly";
                 TB_ID.Background = Brushes.Red;
             }
-            else if (pass != pass_2)
-            {
-                TB_ID.ToolTip = "This field is entered incorrectly";
-            }
-            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
-            {
-                TB_ID.ToolTip = "This field is entered incorrectly";
-                TB_ID.Background = Brushes.Red;
-            }
             else 
             {
                 TB_ID.ToolTip = "";
                 TB_ID.Background = Brushes.Transparent;
                 TB_Password.ToolTip = "";
                 TB_Password.Background = Brushes.Transparent;
-                TB_Password_2.ToolTip = "";
-                TB_Password_2.Background = Brushes.Transparent;
-                TB_EMail.ToolTip = "";
-                TB_EMail.Background = Brushes.Transparent;
 
-                MessageBox.Show("Passt!");
+                User authUser = null;
+                using (AppContext db = new AppContext()) 
+                {
+                    authUser = db.Users.Where(b => b.Login == ID && b.Pass == pass).FirstOrDefault();
+                }
 
-                User user = new User(ID, email, pass);
+                if (authUser != null)
+                    MessageBox.Show("Passt!");
+                else
+                    MessageBox.Show("Error");
+                    
 
-                db.Users.Add(user);
-                db.SaveChanges();
             }
         }
     }
---
+++ UserApp/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp
+{
+    class User
+    {
+        public int id { get; set; }
+
+        private string ID, email,pass;
+
+        public string Login 
+        {
+            get { return ID;  }
+            set { ID = value; }
+        }
+        public string Email 
+        {
+            get { return email;  }
+            set { email = value; }
+        }
+        public string Pass 
+        {
+            get { return pass;  }
+            set { pass = value; }
+        }
+
+        public User() { }
+
+        public User(string login, string email, string pass) 
+        {
+            this.ID = login;
+            this.email = email;
+            this.pass = pass;
+        }
+    }
+}
